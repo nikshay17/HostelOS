9b2c4792928d1fa137031ec57e28a7987661cdad
nikshay17 <nikshaykataria3@gmail.com>
2026-06-28 00:00:00 +0530

    DONE DAY2

--- server/src/app.js
+++ server/src/app.js
@@ -4,11 +4,12 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/auth', require('./routes/auth.routes'));
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-app.use(require('./src/middleware/error.middleware'));
+app.use(require('./middleware/error.middleware'));
 
 module.exports = app;
