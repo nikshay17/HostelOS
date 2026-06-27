e11a3c5ba396c5caafc5f90fdc07a93ecd0609c8
divamkant16 <kantdivam@gmail.com>
2026-06-27 17:37:53 +0530

    Pushing

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.use(require('./src/middleware/error.middleware'));
+
+module.exports = app;
