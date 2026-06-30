4fab399d7ad80c34181bc509b39d84c73d712e7a
divamkant16 <kantdivam@gmail.com>
2026-06-30 17:14:01 +0530

    Ratelimiter added

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -3,10 +3,20 @@ const router = express.Router();
 const { register, login, getMe, createStaff } = require('../controllers/auth.controller');
 const protect = require('../middleware/auth.middleware');
 const authorize = require('../middleware/role.middleware');
+const { registerValidation, loginValidation, createStaffValidation } = require('../middleware/validators/auth.validators');
+const validate = require('../middleware/validate.middleware');
+const { authLimiter } = require('../middleware/rateLimiter.middleware');
 
 router.post('/register', register);           // students only
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.post('/create-staff', protect, authorize('admin'), createStaff); // admin only
+
+router.post('/register', registerValidation, validate, register);
+router.post('/login', loginValidation, validate, login);
+router.post('/create-staff', protect, authorize('admin'), createStaffValidation, validate, createStaff);
+
+router.post('/register', authLimiter, registerValidation, validate, register);
+router.post('/login', authLimiter, loginValidation, validate, login);
 
 module.exports = router;
